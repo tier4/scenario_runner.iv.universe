4a1c283a19ebe89f57ac34d6c68b562c9408b4a3
yamacir-kit <httperror@404-notfound.jp>
2020-06-10 12:26:16 +0900

    Cleanup class Sequence

--- action/scenario_sequence/include/scenario_sequence/sequence.h
+++ action/scenario_sequence/include/scenario_sequence/sequence.h
@@ -5,7 +5,6 @@
 
 #include <yaml-cpp/yaml.h>
 
-#include <scenario_conditions/condition_manager.h>
 #include <scenario_entities/entity_manager.h>
 #include <scenario_expression/expression.h>
 #include <scenario_intersection/intersection_manager.h>
@@ -23,15 +22,9 @@ class Sequence
 
   std::shared_ptr<scenario_sequence::EventManager> event_manager_;
 
-  using condition_pointer
-    = boost::shared_ptr<scenario_conditions::ConditionBase>;
-
   scenario_expression::Expression start_condition_;
-  // std::vector<condition_pointer> conjunctional_conditions_,
-  //                                disjunctional_conditions_;
-  bool ignited_;
 
-  condition_pointer load(const YAML::Node& node) const;
+  bool ignited_;
 
 public:
   Sequence(const scenario_expression::Context&, const YAML::Node&);
--- action/scenario_sequence/src/sequence.cpp
+++ action/scenario_sequence/src/sequence.cpp
@@ -26,54 +26,11 @@ Sequence::Sequence(
     {
       start_condition_ = scenario_expression::read(context_, start_condition);
     }
-    // if (const auto conjunctional_definitions_ {sequence_definition["StartCondition"]["All"]})
-    // {
-    //   for (const auto& each : conjunctional_definitions_)
-    //   {
-    //     conjunctional_conditions_.push_back(load(each));
-    //   }
-    // }
-    // else if (const auto disjunctional_definitions_ {sequence_definition["StartCondition"]["Any"]})
-    // {
-    //   for (const auto& each : disjunctional_definitions_)
-    //   {
-    //     disjunctional_conditions_.push_back(load(each));
-    //   }
-    // }
     else // NOTE: If StartCondition unspecified, the sequence starts unconditionally.
     {
       ignited_ = true;
     }
   }
-}
-
-Sequence::condition_pointer Sequence::load(const YAML::Node& node) const
-{
-  static pluginlib::ClassLoader<scenario_conditions::ConditionBase> loader {
-    "scenario_conditions", "scenario_conditions::ConditionBase"
-  };
-
-  const auto declared_classes {loader.getDeclaredClasses()};
-
-  const auto type {node["Type"].as<std::string>() + "Condition"};
-
-  const auto iter {
-    std::find_if(
-      declared_classes.begin(), declared_classes.end(),
-      [&](const auto& s)
-      {
-        return loader.getName(s) == type;
-      })
-  };
-
-  if (iter != declared_classes.end())
-  {
-    auto instance {loader.createInstance(*iter)};
-    (*instance).configure(node, context_.api);
-    return instance;
-  }
-
-  return {nullptr};
 }
 
 simulation_is Sequence::update(
@@ -82,49 +39,6 @@ simulation_is Sequence::update(
   ROS_INFO_STREAM("\e[1;32m    - Sequence:\e[0m");
   ROS_INFO_STREAM("\e[1;32m        Name: " << name_ << "\e[0m");
   ROS_INFO_STREAM("\e[1;32m        StartCondition:\e[0m");
-
-  // if (not ignited_ and not conjunctional_conditions_.empty())
-  // {
-  //   ROS_INFO_STREAM("\e[1;32m          All:\e[0m");
-  //
-  //   ignited_
-  //     = std::accumulate(
-  //         conjunctional_conditions_.begin(), conjunctional_conditions_.end(),
-  //         true,
-  //         [&](const auto& lhs, const auto& rhs)
-  //         {
-  //           // NOTE: return lhs and (rhs ? (*rhs).update(intersection_manager) : false);
-  //
-  //           const auto result {rhs ? (*rhs).update(intersection_manager) : false};
-  //
-  //           ROS_INFO_STREAM("\e[1;32m            - Type: " << (rhs ? (*rhs).getType() : "Error"));
-  //           ROS_INFO_STREAM("\e[1;32m              Name: " << (rhs ? (*rhs).getName() : "Error"));
-  //           ROS_INFO_STREAM("\e[1;32m              Currently: " << std::boolalpha << result << "\e[0m");
-  //
-  //           return lhs and result;
-  //         });
-  // }
-  // else if (not ignited_ and not disjunctional_conditions_.empty())
-  // {
-  //   ROS_INFO_STREAM("\e[1;32m          Any:\e[0m");
-  //
-  //   ignited_
-  //     = std::accumulate(
-  //         disjunctional_conditions_.begin(), disjunctional_conditions_.end(),
-  //         false,
-  //         [&](const auto& lhs, const auto& rhs)
-  //         {
-  //           // NOTE: return lhs or (rhs ? (*rhs).update(intersection_manager) : false);
-  //
-  //           const auto result {rhs ? (*rhs).update(intersection_manager) : false};
-  //
-  //           ROS_INFO_STREAM("\e[1;32m            - Type: " << (rhs ? (*rhs).getType() : "Error"));
-  //           ROS_INFO_STREAM("\e[1;32m              Name: " << (rhs ? (*rhs).getName() : "Error"));
-  //           ROS_INFO_STREAM("\e[1;32m              Currently: " << std::boolalpha << result << "\e[0m");
-  //
-  //           return lhs or result;
-  //         });
-  // }
 
   if (ignited_ = start_condition_.evaluate(context_))
   {
