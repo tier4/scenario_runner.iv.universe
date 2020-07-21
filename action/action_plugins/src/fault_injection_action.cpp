ab9429160feb1e4af39dae470ed504ea40801fad
yamacir-kit <httperror@404-notfound.jp>
2020-07-21 13:56:06 +0900

    Add dummy FaultInjectionAction

---
+++ action/action_plugins/src/fault_injection_action.cpp
@@ -0,0 +1,47 @@
+#include <action_plugins/fault_injection_action.h>
+
+namespace action_plugins
+{
+
+void FaultInjectionAction::configure(
+  const YAML::Node& node,
+  const std::vector<std::string> actors,
+  const std::shared_ptr<ScenarioAPI>& api_ptr)
+try
+{
+  node_ = node;
+  actors_ = actors;
+  api_ptr_ = api_ptr;
+
+  if (actors_.empty())
+  {
+    SCENARIO_WARNING_ABOUT_NO_ACTORS_SPECIFIED();
+  }
+
+  name_ = read_optional<std::string>(node_, "Name", name_);
+
+  call_with_essential(node_, "Params", [&](const auto& node) mutable
+  {
+    kill_ = read_essential<std::string>(node, "Kill");
+  });
+}
+catch (...)
+{
+  SCENARIO_RETHROW_ERROR_FROM_ACTION_CONFIGURATION();
+}
+
+void FaultInjectionAction::run(
+  const std::shared_ptr<scenario_intersection::IntersectionManager>&)
+{
+  const auto command { "echo 'DEBUG: " + kill_ + "'"};
+
+  if (::system(command.data()))
+  {
+    SCENARIO_ERROR_THROW(CATEGORY(),
+      type_ << "Action failed to execute command \"" << command << "\".");
+  }
+}
+
+} // namespace action_plugins
+
+PLUGINLIB_EXPORT_CLASS(action_plugins::FaultInjectionAction, scenario_actions::EntityActionBase)
