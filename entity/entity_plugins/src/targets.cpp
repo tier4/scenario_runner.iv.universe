b4d53aec4b41ec423e6830eace9b71993c203f02
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2020-10-06 13:50:20 +0900

    remove ROS1 package
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- entity/entity_plugins/src/targets.cpp
+++
@@ -1,5 +0,0 @@
-#include <src/bicycle_entity.cpp>
-#include <src/ego_entity.cpp>
-#include <src/motobike_entity.cpp>
-#include <src/pedestrian_entity.cpp>
-#include <src/vehicle_entity.cpp>
--- scenario_utility/include/scenario_utility/scenario_utility.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCENARIO_UTILS_SCENARIO_UTILS_H_INCLUDED
-#define SCENARIO_UTILS_SCENARIO_UTILS_H_INCLUDED
-
-#include "parse.h"
-#include "converter.h"
-#include "misc.h"
-
-#endif // SCENARIO_UTILS_SCENARIO_UTILS_H_INCLUDED
-
