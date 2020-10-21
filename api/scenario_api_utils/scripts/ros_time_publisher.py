9e46316c3ed0fcb7abe5a90bb52f9425afbf0c5d
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2020-10-21 22:56:07 +0900

    port scenario_api_utils to ROS2
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- api/scenario_api_utils/scripts/ros_time_publisher.py
+++ api/scenario_api_utils/scripts/ros_time_publisher.py
@@ -1,44 +1,49 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-import rospy
+import rclpy
+from rclpy.node import Node
 import time
 from rosgraph_msgs.msg import Clock
 
-
-class RosTimePublisher():
-
+class RosTimePublisher(Node):
     def __init__(self):
-        self.time_rate_ = rospy.get_param("~time_rate", 1.0)
-        # rospy does not have WallTime)
-        self.initial_time_ = rospy.Time.from_sec(time.time())
+        super().__init__('ros_time_publisher')
+        use_sim_time_param = rclpy.parameter.Parameter(
+            "use_sim_time",
+            rclpy.Parameter.Type.BOOL,
+            False)
+        self.set_parameters([use_sim_time_param])
+        self.declare_parameter("time_rate", 1.0)
+        self.time_rate_ = self.get_parameter("time_rate").get_parameter_value().double_value
+        # rclpy does not have WallTime)
+        self.system_clock_ = rclpy.clock.Clock()
+        self.initial_time_ = self.system_clock_.now()
         self.current_ros_time_ = self.initial_time_
-        self.pub_clock_ = rospy.Publisher(
-            "/clock", Clock, tcp_nodelay=True, queue_size=1)
-        rospy.Timer(rospy.Duration(1e-03), self.timerCallback)
+        self.pub_clock_ = self.create_publisher(
+            Clock, "/clock", 1)
+        self.timer_ = self.create_timer(1e-03, self.timerCallback, clock=self.system_clock_)
 
-    def timerCallback(self, event):
+    def timerCallback(self):
         # calculate current ros time with time_rate
-        current_walltime = rospy.Time.from_sec(time.time())
+        current_walltime = self.system_clock_.now()
         dt_walltime = current_walltime - self.initial_time_
-        dt_walltime_ros = dt_walltime * self.time_rate_
-        self.current_ros_time_ = self.initial_time_ + dt_walltime_ros
+        dt_ros_time_ns = dt_walltime.nanoseconds * self.time_rate_
+        self.current_ros_time_ = self.initial_time_ + rclpy.duration.Duration(nanoseconds=dt_ros_time_ns)
 
         # publish clock
         clock_msg = Clock()
-        clock_msg.clock.secs = self.current_ros_time_.secs
-        clock_msg.clock.nsecs = self.current_ros_time_.nsecs
+        clock_msg.clock = self.current_ros_time_.to_msg()
         try:
             self.pub_clock_.publish(clock_msg)
         except:
-            rospy.logerr("cannot publish clock")
+            self.get_logger().error("cannot publish clock")
 
-
-def main():
-    rospy.set_param("/use_sim_time", False)
-    rospy.init_node("ros_time_publisher")
-    RosTimePublisher()
-    rospy.spin()
+def main(args=None):
+    rclpy.init(args=args)
+    node = RosTimePublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
