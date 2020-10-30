2d64a46017e93904b726344b8bc4f2100b5d002a
yamacir-kit <httperror@404-notfound.jp>
2020-10-30 11:29:37 +0900

    Update to show current sequence and current event

--- scenario_expression/include/scenario_expression/expression.h
+++ scenario_expression/include/scenario_expression/expression.h
@@ -78,9 +78,6 @@ public: \
 
 public:
   static std::stringstream json;
-
-  std::string current_sequence,
-              current_event;
 };
 
 /* -----------------------------------------------------------------------------
