f6479ed45e4826c5594ad8231f4305d9339bacdb
yamacir-kit <httperror@404-notfound.jp>
2020-10-30 12:47:54 +0900

    Revert some changes

--- condition/scenario_conditions/include/scenario_conditions/condition_base.h
+++ condition/scenario_conditions/include/scenario_conditions/condition_base.h
@@ -20,6 +20,7 @@
 #include "scenario_api/scenario_api_core.hpp"
 #include "scenario_intersection/intersection_manager.hpp"
 #include "scenario_utility/indentation.hpp"
+#include <limits>
 
 namespace scenario_conditions
 {
@@ -43,6 +44,8 @@ public:
 
   const std::string & getName() const noexcept {return name_;}
 
+  double getValue() const noexcept { return value_; }
+
   const auto& rename(const std::string& new_name)
   {
     return name_ = new_name;
@@ -52,16 +55,14 @@ public:
 
   const std::string & getType() const noexcept {return type_;}
 
-  // friend std::ostream& operator <<(std::ostream& os, const ConditionBase& datum)
-  // {
-  //   return os << indent
-  //             << "{ Name: \x1b[36m"
-  //             << std::quoted(datum.name_)
-  //             << "\x1b[0m, Value: "
-  //             << (datum.result_ ? "\x1b[32m" : "\x1b[31m")
-  //             << std::boolalpha << datum.result_
-  //             << "\x1b[0m },\n";
-  // }
+  friend std::ostream& operator <<(std::ostream& os, const ConditionBase& datum)
+  {
+    return os << indent
+              << "{ Name: \x1b[36m" << std::quoted(datum.getName()) << "\x1b[0m"
+              << ", Value: " << datum.getValue()
+              << ", Result: " << (datum.getResult() ? "\x1b[32m" : "\x1b[31m") << std::boolalpha << datum.getResult() << "\x1b[0m"
+              << "},\n";
+  }
 
 protected:
   std::shared_ptr<ScenarioAPI> api_ptr_;
@@ -70,6 +71,8 @@ protected:
   bool configured_ = false;
   bool keep_ = false;
   bool result_ = false;
+
+  double value_ { std::numeric_limits<double>::quiet_NaN() };
 
   std::string type_;
   std::string name_;
