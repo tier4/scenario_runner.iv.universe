ffb7018b420cb5655fbeec350ea0e6d2c8056b4f
yamacir-kit <httperror@404-notfound.jp>
2020-11-06 17:14:34 +0900

    Add member function 'info' to class SimpleLogger

--- logger/scenario_logger/include/scenario_logger/simple_logger.hpp
+++ logger/scenario_logger/include/scenario_logger/simple_logger.hpp
@@ -2,13 +2,37 @@
 #define INCLUDED_SCENARIO_LOGGER_SIMPLE_LOGGER_HPP
 
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 
 namespace scenario_logger { inline namespace simple
 {
+  decltype(auto) endlog(std::ostream& os)
+  {
+    return os << "\" }," << std::endl;
+  }
+
   struct SimpleLogger
     : public std::ofstream
   {
+    struct Header
+    {
+      const std::string level;
+
+      explicit Header(const std::string& level)
+        : level { level }
+      {}
+
+      friend decltype(auto) operator <<(std::ostream& os, const Header& header)
+      {
+        return os << "{ \"level\": " << std::quoted(header.level) << ", \"message\": \"";
+      }
+    };
+
+    decltype(auto) info()
+    {
+      return (*this) << Header("info");
+    }
   };
 
   extern SimpleLogger& slog;
