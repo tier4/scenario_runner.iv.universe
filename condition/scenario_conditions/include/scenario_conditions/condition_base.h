d30a725c89fb3620806060424fc71609a88592ae
Jilada Eccleston <jilada.eccleston@tier4.jp>
2020-11-16 16:11:30 +0900

    Convert base implementation

--- condition/scenario_conditions/include/scenario_conditions/condition_base.h
+++ condition/scenario_conditions/include/scenario_conditions/condition_base.h
@@ -3,8 +3,10 @@
 
 #include <yaml-cpp/yaml.h>
 
-#include <scenario_api/scenario_api_core.h>
-#include <scenario_intersection/intersection_manager.h>
+#include <boost/lexical_cast.hpp>
+
+// #include <scenario_api/scenario_api_core.h>
+// #include <scenario_intersection/intersection_manager.h>
 
 namespace scenario_conditions
 {
@@ -21,25 +23,26 @@ public:
   template <typename T>
   using Comparator = std::function<bool(const T &, const T &)>;
 
-  virtual bool update(const std::shared_ptr<scenario_intersection::IntersectionManager> &) = 0;
-  virtual bool configure(YAML::Node node, std::shared_ptr<ScenarioAPI> api_ptr) = 0;
+  // virtual bool update(const std::shared_ptr<scenario_intersection::IntersectionManager> &) = 0;
+  virtual bool configure(YAML::Node node) = 0;
+  // virtual bool configure(YAML::Node node, std::shared_ptr<ScenarioAPI> api_ptr) = 0;
 
   const std::string & getName() const noexcept { return name_; }
 
-  const bool getResult() const noexcept { return result_; }
+  bool getResult() const noexcept { return result_; }
 
   const std::string & getType() const noexcept { return type_; }
 
 protected:
-  std::shared_ptr<ScenarioAPI> api_ptr_;
+  // std::shared_ptr<ScenarioAPI> api_ptr_;
   YAML::Node node_;
 
   bool configured_ = false;
   bool keep_ = false;
   bool result_ = false;
 
-  std::string name_;
   std::string type_;
+  std::string name_;
 };
 
 }  // namespace scenario_conditions
