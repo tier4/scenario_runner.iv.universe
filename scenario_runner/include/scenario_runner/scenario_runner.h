51e044e1e16cfa4b2afea8683e85ddb45d1aedb3
Yunus Emre Caliskan <yunus.ec@gmail.com>
2020-11-20 16:28:32 +0100

    Port scenario runner to ros2

--- scenario_runner/include/scenario_runner/scenario_runner.h
+++ scenario_runner/include/scenario_runner/scenario_runner.h
@@ -3,7 +3,7 @@
 
 #include <memory>
 
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 
 #include <scenario_expression/expression.h>
 #include <scenario_intersection/intersection_manager.h>
@@ -14,10 +14,10 @@
 
 namespace scenario_runner
 {
-class ScenarioRunner
+class ScenarioRunner : public rclcpp::Node
 {
 public:
-  ScenarioRunner(ros::NodeHandle nh, ros::NodeHandle pnh);
+  ScenarioRunner();
   void run();
 
   double current_mileage() const
@@ -28,9 +28,7 @@ public:
   simulation_is currently;
 
 private:
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_;
-  ros::Timer timer_;
+  rclcpp::TimerBase::SharedPtr timer_;
 
   std::string scenario_path_;
 
@@ -46,7 +44,7 @@ private:
   std::shared_ptr<scenario_sequence::SequenceManager> sequence_manager_;
   std::shared_ptr<scenario_intersection::IntersectionManager> intersection_manager_;
 
-  void update(const ros::TimerEvent & event);
+  void update();
 };
 
 }  // namespace scenario_runner
--- scenario_runner/src/scenario_runner_node.cpp
+++ scenario_runner/src/scenario_runner_node.cpp
@@ -2,7 +2,7 @@
 #include <boost/cstdlib.hpp>
 #include <exception>
 #include <glog/logging.h>
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 #include <scenario_logger/logger.h>
 #include <scenario_runner/scenario_runner.h>
 #include <scenario_runner/scenario_terminater.h>
@@ -21,41 +21,37 @@ int main(int argc, char * argv[]) try
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureFunction(&failureCallback);
 
-  ros::init(argc, argv, "scenario_runner_node");
-  ros::NodeHandle nh;
-  ros::NodeHandle pnh("~");
+  rclcpp::init(argc, argv);
+  /*
+ * setup scenario runner
+ */
+  SCENARIO_INFO_STREAM(CATEGORY("simulation", "progress"), "ScenarioRunner instantiated.");
+  const auto runner_ptr = std::make_shared<scenario_runner::ScenarioRunner>();
 
-  scenario_logger::log.setStartDatetime(ros::Time::now());
+  scenario_logger::log.setStartDatetime(runner_ptr->now());
   SCENARIO_LOG_STREAM(CATEGORY("simulation", "progress"), "Logging started.");
 
-  std::string scenario_id;
-  pnh.getParam("scenario_id", scenario_id);
+  std::string scenario_id{runner_ptr->declare_parameter("scenario_id").get<std::string>()};
   scenario_logger::log.setScenarioID(scenario_id);
 
-  std::string log_output_path;
-  pnh.getParam("log_output_path", log_output_path);
+  std::string log_output_path{runner_ptr->declare_parameter("log_output_path").get<std::string>()};
   scenario_logger::log.setLogOutputPath(log_output_path);
 
   SCENARIO_INFO_STREAM(CATEGORY(), "Sleep for 10 seconds.");
   std::this_thread::sleep_for(std::chrono::seconds { 10 });
   SCENARIO_INFO_STREAM(CATEGORY(), "Wake-up.");
 
-  /*
-   * setup scenario runner
-   */
-  scenario_runner::ScenarioRunner runner(nh, pnh);
-  SCENARIO_INFO_STREAM(CATEGORY("simulation", "progress"), "ScenarioRunner instantiated.");
 
   /*
    * start simulation
    */
-  for (runner.run(); ros::ok(); ros::spinOnce())
+  for (runner_ptr->run(); rclcpp::ok(); rclcpp::spin_some(runner_ptr))
   {
-    static auto previously{runner.currently};
+    static auto previously{runner_ptr->currently};
 
-    if (previously != runner.currently)
+    if (previously != runner_ptr->currently)
     {
-      switch (previously = runner.currently)
+      switch (previously = runner_ptr->currently)
       {
       case simulation_is::succeeded:
         SCENARIO_INFO_STREAM(CATEGORY("simulator", "endcondition"), "simulation succeeded");
@@ -74,13 +70,13 @@ int main(int argc, char * argv[]) try
       }
     }
 
-    terminator.update_mileage(runner.current_mileage());
+    terminator.update_mileage(runner_ptr->current_mileage());
 
     terminator.update_duration(
-      (ros::Time::now() - scenario_logger::log.begin()).toSec());
+      (runner_ptr->now() - scenario_logger::log.begin()).seconds());
   }
 
-  if (runner.currently == simulation_is::ongoing)
+  if (runner_ptr->currently == simulation_is::ongoing)
   {
     SCENARIO_INFO_STREAM(CATEGORY(), "Simulation aborted.");
     scenario_logger::log.write();
