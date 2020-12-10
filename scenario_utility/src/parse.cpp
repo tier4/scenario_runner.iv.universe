39abcd0b2989e27833a8166cdbf2518e0d13c854
Nikolai Morin <nnmmgit@gmail.com>
2020-12-10 05:31:04 +0100

    Enable lints in scenario_utility (#41)

--- scenario_utility/src/parse.cpp
+++ scenario_utility/src/parse.cpp
@@ -14,20 +14,22 @@
 
 #include "scenario_utility/parse.hpp"
 
+#include <string>
+#include <vector>
+
 inline namespace scenario_utility
 {
 inline namespace parse
 {
-
-#define DEFINE_READ_ESSENTIAL_SPECIALIZATION(TYPENAME)                         \
-  READ_ESSENTIAL_SPECIALIZED_SIGNATURE(TYPENAME)                               \
-  {                                                                            \
-    return call_with_essential(node, key, read_as<TYPENAME>);                  \
-  }                                                                            \
+#define DEFINE_READ_ESSENTIAL_SPECIALIZATION(TYPENAME) \
+  READ_ESSENTIAL_SPECIALIZED_SIGNATURE(TYPENAME) \
+  { \
+    return call_with_essential(node, key, read_as<TYPENAME>); \
+  } \
   static_assert(true, "semicolon required after this macro")
 
-template <>
-geometry_msgs::msg::Point read_as<geometry_msgs::msg::Point>(const YAML::Node& node)
+template<>
+geometry_msgs::msg::Point read_as<geometry_msgs::msg::Point>(const YAML::Node & node)
 {
   geometry_msgs::msg::Point point {};
 
@@ -40,8 +42,8 @@ geometry_msgs::msg::Point read_as<geometry_msgs::msg::Point>(const YAML::Node& n
 
 DEFINE_READ_ESSENTIAL_SPECIALIZATION(geometry_msgs::msg::Point);
 
-template <>
-geometry_msgs::msg::Quaternion read_as<geometry_msgs::msg::Quaternion>(const YAML::Node& node)
+template<>
+geometry_msgs::msg::Quaternion read_as<geometry_msgs::msg::Quaternion>(const YAML::Node & node)
 {
   geometry_msgs::msg::Quaternion quaternion {};
 
@@ -55,8 +57,8 @@ geometry_msgs::msg::Quaternion read_as<geometry_msgs::msg::Quaternion>(const YAM
 
 DEFINE_READ_ESSENTIAL_SPECIALIZATION(geometry_msgs::msg::Quaternion);
 
-template <>
-geometry_msgs::msg::Pose read_as<geometry_msgs::msg::Pose>(const YAML::Node& node)
+template<>
+geometry_msgs::msg::Pose read_as<geometry_msgs::msg::Pose>(const YAML::Node & node)
 {
   geometry_msgs::msg::Pose pose {};
 
@@ -68,8 +70,8 @@ geometry_msgs::msg::Pose read_as<geometry_msgs::msg::Pose>(const YAML::Node& nod
 
 DEFINE_READ_ESSENTIAL_SPECIALIZATION(geometry_msgs::msg::Pose);
 
-template <>
-geometry_msgs::msg::PoseStamped read_as<geometry_msgs::msg::PoseStamped>(const YAML::Node& node)
+template<>
+geometry_msgs::msg::PoseStamped read_as<geometry_msgs::msg::PoseStamped>(const YAML::Node & node)
 {
   geometry_msgs::msg::PoseStamped pose_stamped {};
 
@@ -77,7 +79,8 @@ geometry_msgs::msg::PoseStamped read_as<geometry_msgs::msg::PoseStamped>(const Y
   // pose_stamped.header.stamp = ros::Time::now();
   // pose_stamped.pose = read_essential<geometry_msgs::msg::Pose>(node, "Pose");
   pose_stamped.pose.position = read_essential<geometry_msgs::msg::Point>(node, "Position");
-  pose_stamped.pose.orientation = read_essential<geometry_msgs::msg::Quaternion>(node, "Orientation");
+  pose_stamped.pose.orientation =
+    read_essential<geometry_msgs::msg::Quaternion>(node, "Orientation");
 
   return pose_stamped;
 }
@@ -87,14 +90,12 @@ DEFINE_READ_ESSENTIAL_SPECIALIZATION(geometry_msgs::msg::PoseStamped);
 std::vector<std::string> split(std::string s)
 {
   std::vector<std::string> v;
-  std::stringstream ss{ s };
+  std::stringstream ss{s};
   std::string buf;
-  while (std::getline(ss, buf, ','))
-  {
+  while (std::getline(ss, buf, ',')) {
     v.push_back(buf);
   }
   return v;
 }
-
 }  // namespace parse
 }  // namespace scenario_utility
