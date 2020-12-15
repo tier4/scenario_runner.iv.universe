3ce04a43b7722dc4e84a75d35468ab6d25bfadbe
Nikolai Morin <nikolai.morin@apex.ai>
2020-12-15 13:28:00 +0100

    Enable lints in scenario_expression

--- scenario_expression/src/expression.cpp
+++ scenario_expression/src/expression.cpp
@@ -17,50 +17,39 @@
 namespace scenario_expression
 {
 
-Expression read(Context& context, const YAML::Node& node)
+Expression read(Context & context, const YAML::Node & node)
 {
-  if (node.IsScalar())
-  {
-    SCENARIO_ERROR_THROW(CATEGORY(),
+  if (node.IsScalar()) {
+    SCENARIO_ERROR_THROW(
+      CATEGORY(),
       "syntax-error: Scalar cannot appear in this context.\n\n" << node << "\n");
-  }
-  else if (node.IsSequence())
-  {
-    SCENARIO_ERROR_THROW(CATEGORY(),
+  } else if (node.IsSequence()) {
+    SCENARIO_ERROR_THROW(
+      CATEGORY(),
       "syntax-error: Sequence cannot appear in this context.\n\n" << node << "\n");
-  }
-  else if (node.IsMap()) // is <keyword>
-  {
-    if (const auto all { node["All"] }) // NOTE: should be 'and'
-    {
+  } else if (node.IsMap()) { // is <keyword>
+    if (const auto all {node["All"]}) { // NOTE: should be 'and'
       return Expression::make<And>(context, all);
-    }
-    else if (const auto any { node["Any"] }) // NOTE: should be 'or'
-    {
+    } else if (const auto any {node["Any"]}) { // NOTE: should be 'or'
       return Expression::make<Or>(context, any);
-    }
-    else if (const auto type { node["Type"] }) // <procedure call>
-    {
-      if (const auto params { node["Params"] }) // <action call>
-      {
-        SCENARIO_ERROR_THROW(CATEGORY(),
+    } else if (const auto type {node["Type"]}) { // <procedure call>
+      if (const auto params {node["Params"]}) { // <action call>
+        SCENARIO_ERROR_THROW(
+          CATEGORY(),
           "syntax-error: Procedure-call cannot appear in this context.\n\n" << node << "\n");
-      }
-      else // <predicate call>
-      {
+      } else { // <predicate call>
         return Expression::make<Predicate>(context, node);
       }
-    }
-    else
-    {
-      SCENARIO_ERROR_THROW(CATEGORY(),
+    } else {
+      SCENARIO_ERROR_THROW(
+        CATEGORY(),
         "syntax-error: Malformed clause.\n\n" << node << "\n");
     }
-  }
-  else
-  {
-    SCENARIO_WARN_STREAM(CATEGORY(),
-      "syntax-error: An unexpected expression has appeared. It is treated as an empty expression.\n\n" << node << "\n");
+  } else {
+    SCENARIO_WARN_STREAM(
+      CATEGORY(),
+      "syntax-error: An unexpected expression has appeared. It is treated as an empty expression.\n\n" << node <<
+        "\n");
     return {};
   }
 }
@@ -70,9 +59,10 @@ Expression read(Context& context, const YAML::Node& node)
 namespace std
 {
 
-template <>
-void swap(scenario_expression::Expression& lhs,
-          scenario_expression::Expression& rhs)
+template<>
+void swap(
+  scenario_expression::Expression & lhs,
+  scenario_expression::Expression & rhs)
 {
   lhs.swap(rhs);
 }
