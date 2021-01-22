d69357d158842f4dfc528149bc5ce4ef64d1cebd
Kosuke Murakami <kosuke.murakami@tier4.jp>
2021-01-22 15:02:40 +0900

    update tmp_launch
    Signed-off-by: Kosuke Murakami <kosuke.murakami@tier4.jp>

--- tmp_launch/tmp_launch/tmp_launch.py
+++ tmp_launch/tmp_launch/tmp_launch.py
@@ -21,13 +21,14 @@ def main():
 
     #TODO: Edit
     # scenario_path = "/home/kosuke/scenario_tests/odd3/scenario/UC-003-0003/PSim_1/UC-003-0003_PSim_1_case1.yaml"
-    scenario_path = "/home/kosuke/scenario_tests/odd2/scenario/UC-001-0003/PSim_1/UC-001-0003_PSim_1_case1.yaml"
+    scenario_path = "/home/kosuke/scenario_tests/odd2/scenario/UC-001-0003/PSim_1/UC-001-0003_PSim_1_case1.yaml"  #hamakita
+    # scenario_path = "/home/kosuke/scenario_tests/odd3/scenario/UC-008-0005/PSim_1/UC-008-0005_PSim_1_case1.yaml"  #nishishinjuku_aisan
     log_output_base_dir = "/home/kosuke/AutowareArchitectureProposal/src/simulator/scenario_runner/tmp_launch"
     scenario_runner_args = {
                     'scenario_id': scenario_id,
                     'scenario_path': str(scenario_path),
                     'log_output_path': f'{log_output_base_dir}/{scenario_id}.json',
-                    'json_dump_path': f'{log_output_base_dir}/result-of-{scenario_path}.json',
+                    'json_dump_path': f'{log_output_base_dir}/result-of-{scenario_id}.json',
                     'camera_frame_id': 'camera5/camera_optical_link',
                     'initial_engage_state': False
                 }
@@ -45,6 +46,8 @@ def main():
             ("input/autoware_state", "/autoware/state"),
             ("input/vehicle_twist", "/vehicle/status/twist"),
             ("input/signal_command", "/vehicle/status/turn_signal"),
+            ("input/engage","/simulation/npc_simulator/engage"),
+            ("input/ego_vehicle_pose","/current_pose"),
             ("output/start_point", "/initialpose"),
             ("output/initial_velocity", "/initialtwist"),
             ("output/goal_point", "/planning/mission_planning/goal"),
@@ -56,8 +59,10 @@ def main():
             ("output/object_info", "/simulation/npc_simulator/object_info"),
             ("output/traffic_detection_result", "/perception/traffic_light_recognition/traffic_light_states"),  # noqa: E501
             ("output/lane_change_permission", "/planning/scenario_planning/lane_driving/lane_change_approval"),  # noqa: E501
-        ],
-        # sigterm_timeout=???, TODO
+            ("output/dynamic_object_info" ,"/simulation/dummy_perception_publisher/object_info"),
+            ("output/debug_object_info","/simulation/npc_simulator/ground_truth_object_info")]
+    #  ),
+        # sigterm_timeout=???, TO)
     )
 
     # launch_path= "/home/kosuke/AutowareArchitectureProposal/src/autoware/launcher/autoware_launch/launch/planning_simulator.launch.xml"
