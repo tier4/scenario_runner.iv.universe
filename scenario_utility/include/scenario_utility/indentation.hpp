70dbee19fc6a40888b9875b79655b276440fdf03
Kosuke Murakami <kosuke.murakami@tier4.jp>
2021-02-10 15:48:57 +0900

    fix lint

--- scenario_utility/include/scenario_utility/indentation.hpp
+++ scenario_utility/include/scenario_utility/indentation.hpp
@@ -1,5 +1,19 @@
-#ifndef INCLUDED_SCENARIO_UTILITY_INDENTATION_HPP
-#define INCLUDED_SCENARIO_UTILITY_INDENTATION_HPP
+// Copyright 2020 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SCENARIO_UTILITY__INDENTATION_HPP_
+#define SCENARIO_UTILITY__INDENTATION_HPP_
 
 #include <cstddef>
 #include <iostream>
@@ -7,7 +21,6 @@
 
 inline namespace scenario_utility
 {
-
 struct Indentation
 {
   std::size_t depth {0};
@@ -34,7 +47,5 @@ static struct IndentationInitializer
   IndentationInitializer();
   ~IndentationInitializer();
 } indentation_initializer;
-
 }  // namespace scenario_utility
-
-#endif // INCLUDED_SCENARIO_UTILITY_INDENTATION_HPP
+#endif  // SCENARIO_UTILITY__INDENTATION_HPP_
--- scenario_utility/src/indentation.cpp
+++ scenario_utility/src/indentation.cpp
@@ -1,13 +1,27 @@
+// Copyright 2020 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <scenario_utility/indentation.hpp>
+#include <string>
 
 inline namespace scenario_utility
 {
-
-static auto schwarz_counter {0};
+static auto schwarz_counter{0};
 
 static typename std::aligned_storage<sizeof(Indentation), alignof(Indentation)>::type memory;
 
-Indentation & indent {reinterpret_cast<Indentation &>(memory)};
+Indentation & indent{reinterpret_cast<Indentation &>(memory)};
 
 std::ostream & operator<<(std::ostream & os, const Indentation & datum)
 {
@@ -27,5 +41,4 @@ IndentationInitializer::~IndentationInitializer()
     indent.~Indentation();
   }
 }
-
-} // namespace scenario_utility
+}  // namespace scenario_utility
