81b72048f62ae668e94ab2c0311c9434cc3b3c73
Takagi, Isamu <isamu.takagi@tier4.jp>
2021-02-10 21:29:18 +0900

    Fix for linter

--- logger/scenario_logger/include/scenario_logger/simple_logger.hpp
+++ logger/scenario_logger/include/scenario_logger/simple_logger.hpp
@@ -1,92 +1,103 @@
-#ifndef INCLUDED_SCENARIO_LOGGER_SIMPLE_LOGGER_HPP
-#define INCLUDED_SCENARIO_LOGGER_SIMPLE_LOGGER_HPP
+// Copyright 2020 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SCENARIO_LOGGER__SIMPLE_LOGGER_HPP_
+#define SCENARIO_LOGGER__SIMPLE_LOGGER_HPP_
 
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <string>
 
-namespace scenario_logger { inline namespace simple
+namespace scenario_logger
 {
-  decltype(auto) elog(std::ostream& os)
-  {
-    return os << "\" }" << std::endl;
-  }
+inline namespace simple
+{
+decltype(auto) elog(std::ostream & os)
+{
+  return os << "\" }" << std::endl;
+}
 
-  struct SimpleLogger
-    : public std::ofstream
+struct SimpleLogger
+  : public std::ofstream
+{
+  struct Header
   {
-    struct Header
+    const std::string level;
+
+    explicit Header(const std::string & level)
+    : level{level}
+    {}
+
+    friend decltype(auto) operator<<(std::ostream & os, const Header & header)
     {
-      const std::string level;
-
-      explicit Header(const std::string& level)
-        : level { level }
-      {}
-
-      friend decltype(auto) operator <<(std::ostream& os, const Header& header)
-      {
-        return os << "{ \"level\": " << std::quoted(header.level) << ", \"message\": \"";
-      }
-    };
-
-    decltype(auto) info()  { return (*this) << Header("info"); }
-    decltype(auto) error() { return (*this) << Header("error"); }
+      return os << "{ \"level\": " << std::quoted(header.level) << ", \"message\": \"";
+    }
   };
 
-  extern SimpleLogger& slog;
+  decltype(auto) info() {return (*this) << Header("info");}
+  decltype(auto) error() {return (*this) << Header("error");}
+};
 
-  static struct SimpleLoggerInitializer
-  {
-    SimpleLoggerInitializer();
-    ~SimpleLoggerInitializer();
-  } initializer;
-}} // namespace scenario_logger
+extern SimpleLogger & slog;
 
-
-#define LOG_SIMPLE(...)                                                        \
-do                                                                             \
-{                                                                              \
-  using scenario_logger::slog;                                                 \
-  using scenario_logger::elog;                                                 \
-                                                                               \
-  slog. __VA_ARGS__ << elog;                                                   \
-}                                                                              \
-while (false)
+static struct SimpleLoggerInitializer
+{
+  SimpleLoggerInitializer();
+  ~SimpleLoggerInitializer();
+} initializer;
+}  // namespace simple
+}  // namespace scenario_logger
 
 
-#define LOG_SIMPLE_ONCE(...)                                                   \
-do                                                                             \
-{                                                                              \
-  using scenario_logger::slog;                                                 \
-  using scenario_logger::elog;                                                 \
-                                                                               \
-  static auto logged { false };                                                \
-                                                                               \
-  if (not logged)                                                              \
-  {                                                                            \
-    slog. __VA_ARGS__ << elog;                                                 \
-    logged = true;                                                             \
-  }                                                                            \
-}                                                                              \
-while (false)
+#define LOG_SIMPLE(...)                                                     /**/ \
+  do {                                                                      /**/ \
+    using scenario_logger::slog;                                            /**/ \
+    using scenario_logger::elog;                                            /**/ \
+                                                                            /**/ \
+    slog.__VA_ARGS__ << elog;                                               /**/ \
+  }                                                                         /**/ \
+  while (false)
 
+#define LOG_SIMPLE_ONCE(...)                                                /**/ \
+  do {                                                                      /**/ \
+    using scenario_logger::slog;                                            /**/ \
+    using scenario_logger::elog;                                            /**/ \
+                                                                            /**/ \
+    static auto logged {false};                                             /**/ \
+                                                                            /**/ \
+    if (!logged) {                                                          /**/ \
+      slog.__VA_ARGS__ << elog;                                             /**/ \
+      logged = true;                                                        /**/ \
+    }                                                                       /**/ \
+  }                                                                         /**/ \
+  while (false)
 
-#define LOG_TOGGLE(TITLE, VARIABLE)                                            \
-do                                                                             \
-{                                                                              \
-  using scenario_logger::slog;                                                 \
-  using scenario_logger::elog;                                                 \
-                                                                               \
-  static auto previous {                                                       \
-    ((slog.info() << TITLE ": " << VARIABLE << elog), VARIABLE)                \
-  };                                                                           \
-                                                                               \
-  if (previous != VARIABLE)                                                    \
-  {                                                                            \
-    slog.info() << TITLE ": " << previous << " => " << VARIABLE << elog;       \
-    previous = VARIABLE;                                                       \
-  }                                                                            \
-}                                                                              \
-while (false)
+#define LOG_TOGGLE(TITLE, VARIABLE)                                         /**/ \
+  do {                                                                      /**/ \
+    using scenario_logger::slog;                                            /**/ \
+    using scenario_logger::elog;                                            /**/ \
+                                                                            /**/ \
+    static auto previous {                                                  /**/ \
+      ((slog.info() << TITLE ": " << VARIABLE << elog), VARIABLE)           /**/ \
+    };                                                                      /**/ \
+                                                                            /**/ \
+    if (previous != VARIABLE) {                                             /**/ \
+      slog.info() << TITLE ": " << previous << " => " << VARIABLE << elog;  /**/ \
+      previous = VARIABLE;                                                  /**/ \
+    }                                                                       /**/ \
+  }                                                                         /**/ \
+  while (false)  // NOLINT
 
-#endif // INCLUDED_SCENARIO_LOGGER_SIMPLE_LOGGER_HPP
+#endif  // SCENARIO_LOGGER__SIMPLE_LOGGER_HPP_
--- logger/scenario_logger/src/simple_logger.cpp
+++ logger/scenario_logger/src/simple_logger.cpp
@@ -1,30 +1,43 @@
+// Copyright 2020 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <scenario_logger/simple_logger.hpp>
 
-namespace scenario_logger { inline namespace simple
+namespace scenario_logger
 {
-
-static std::size_t schwarz_counter { 0 };
+inline namespace simple
+{
+static std::size_t schwarz_counter {0};
 
 static typename std::aligned_storage<
   sizeof(SimpleLogger), alignof(SimpleLogger)
 >::type memory;
 
-SimpleLogger& slog { reinterpret_cast<SimpleLogger&>(memory) };
+SimpleLogger & slog {reinterpret_cast<SimpleLogger &>(memory)};
 
 SimpleLoggerInitializer::SimpleLoggerInitializer()
 {
-  if (schwarz_counter++ == 0)
-  {
+  if (schwarz_counter++ == 0) {
     new (&slog) SimpleLogger();
   }
 }
 
 SimpleLoggerInitializer::~SimpleLoggerInitializer()
 {
-  if (--schwarz_counter == 0)
-  {
+  if (--schwarz_counter == 0) {
     slog.~SimpleLogger();
   }
 }
-
-}} // namespace scenario_logger
+}  // namespace simple
+}  // namespace scenario_logger
