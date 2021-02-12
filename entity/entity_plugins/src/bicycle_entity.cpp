61f6482b9584b5657f46919f0ca7e14a14ac3467
Kosuke Murakami <kosuke.murakami@tier4.jp>
2021-02-12 10:53:05 +0900

    rename to ros1 style
    Signed-off-by: Kosuke Murakami <kosuke.murakami@tier4.jp>

--- entity/entity_plugins/src/bicycle_entity.cpp
+++ entity/entity_plugins/src/bicycle_entity.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "entity_plugins/bicycle_entity.hpp"
+#include "entity_plugins/bicycle_entity.h"
 
 namespace entity_plugins
 {
